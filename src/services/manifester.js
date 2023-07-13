09a4fcc0fc6d692e2737b15392e4ed06e6d2eb25
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2023-07-13 14:03:32 -0600

    Update manifester.js
    Uses expressJS for routing and request

--- src/services/manifester.js
+++ src/services/manifester.js
@@ -1,145 +1,167 @@
-const http = require('http');
-const hostname = 'localhost';
-const port = 3000;
+const express = require('express');
+const app = express();
 const url = require('url');
+const request = require('request-promise');
+
 const gsheetkey = 'AIzaSyDbKq9njfiQa3UDGzRaaG0YaQOT0kYNoFQ';
 const aprsfikey = '161937.xJdpU4VkxgfJ2EA';
 
-const server = http.createServer((req, res) => {
-	var request = require('request-promise');
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}/`);
+});
 
+app.get('/', (req, res) => {
 	var Manifester = {
 		g: null,
 		r: null,
-		getSmapGeo: function(id) {
+		getSmapGeo: function (id) {
 			return request({
-				"method":"GET", 
-				"uri": "https://raw.githubusercontent.com/hock/smapdata/master/data/"+id+".geojson",
-				"json": true
+				method: 'GET',
+				uri: 'https://raw.githubusercontent.com/hock/smapdata/master/data/' + id + '.geojson',
+				json: true,
 			});
 		},
-		getSmapGraph: function(id) {
+		getSmapGraph: function (id) {
 			return request({
-				"method":"GET", 
-				"uri": "https://raw.githubusercontent.com/hock/smapdata/master/data/"+id+".json",
-				"json": true
+				method: 'GET',
+				uri: 'https://raw.githubusercontent.com/hock/smapdata/master/data/' + id + '.json',
+				json: true,
 			});
 		},
-		getGoogleOverview: function(id) {
+		getGoogleOverview: function (id) {
 			return request({
-				"uri": "https://sheets.googleapis.com/v4/spreadsheets/" + id + "/values/Overview?key=" + gsheetkey,
-				"json": true
+				uri: 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/Overview?key=' + gsheetkey,
+				json: true,
 			});
 		},
-		getGoogleList: function(id) {
+		getGoogleList: function (id) {
 			return request({
-				"method":"GET", 
-				"uri": "https://sheets.googleapis.com/v4/spreadsheets/" + id + "/values/List?key=" + gsheetkey,
-				"json": true
+				method: 'GET',
+				uri: 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/List?key=' + gsheetkey,
+				json: true,
 			});
 		},
-		
-		getMarineTrafficOverview: function() {
+		getMarineTrafficOverview: function () {
 			return request({
-				"method":"GET", 
-				"uri": "http://services.marinetraffic.com/api/exportvessels/cc503f48eb2c8e49549cc56de3c7059c4b042931/timespan:4/protocol:json",
-				"json": true
+				method: 'GET',
+				uri: 'http://services.marinetraffic.com/api/exportvessels/cc503f48eb2c8e49549cc56de3c7059c4b042931/timespan:4/protocol:json',
+				json: true,
 			});
 		},
-		
-		getAprsFi: function(vid) {
+		getAprsFi: function (vid) {
 			return request({
-				"method":"GET", 
-				"uri": "https://api.aprs.fi/api/get?name="+vid+"&what=loc&apikey="+aprsfikey+"&format=json",
-			    "headers": {
-			           'User-Agent': 'manifest/0.2 (+https://manifest.supplystudies.com/)'
-			    },
-				"json": true
+				method: 'GET',
+				uri: 'https://api.aprs.fi/api/get?name=' + vid + '&what=loc&apikey=' + aprsfikey + '&format=json',
+				headers: {
+					'User-Agent': 'manifest/0.2 (+https://manifest.supplystudies.com/)',
+				},
+				json: true,
 			});
-		}
-		
+		},
 	};
-	
+
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
-	
-	if (query.type == "smap" || query.type == "gsheet") {
+
+	if (query.type == 'smap' || query.type == 'gsheet') {
 		var gprocessor = null;
 		var rprocessor = null;
-	
-		if(query.type == "smap") { gprocessor = Manifester.getSmapGeo; rprocessor = Manifester.getSmapGraph; }	
-		if(query.type == "gsheet") { gprocessor = Manifester.getGoogleOverview; rprocessor = Manifester.getGoogleList; }
 
-		gprocessor(query.id).then(function(result) {
-		  	Manifester.g = result; 
-			rprocessor(query.id).then(function(result) {
-				Manifester.r = result;
-				res.statusCode = 200;
+		if (query.type == 'smap') {
+			gprocessor = Manifester.getSmapGeo;
+			rprocessor = Manifester.getSmapGraph;
+		}
+		if (query.type == 'gsheet') {
+			gprocessor = Manifester.getGoogleOverview;
+			rprocessor = Manifester.getGoogleList;
+		}
 
+		gprocessor(query.id)
+			.then(function (result) {
+				Manifester.g = result;
+				rprocessor(query.id)
+					.then(function (result) {
+						Manifester.r = result;
+						res.statusCode = 200;
+
+						res.setHeader('Access-Control-Allow-Origin', '*');
+						res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+						res.setHeader('Access-Control-Allow-Methods', 'GET');
+						res.setHeader('Content-Type', 'application/json');
+
+						console.log('done');
+						res.end('{"g": ' + JSON.stringify(Manifester.g) + ', "r": ' + JSON.stringify(Manifester.r) + '}');
+					})
+					.catch(function (err) {
+						console.log('failure!');
+
+						res.statusCode = 404;
+						res.setHeader('Access-Control-Allow-Origin', '*');
+						res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+						res.setHeader('Access-Control-Allow-Methods', 'GET');
+						res.setHeader('Content-Type', 'application/json');
+						res.end('{"not found"}');
+					});
+			})
+			.catch(function (err) {
+				console.log('failed');
+
+				res.statusCode = 404;
 				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+				res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 				res.setHeader('Access-Control-Allow-Methods', 'GET');
 				res.setHeader('Content-Type', 'application/json');
-			
-				console.log("process success!");
-				res.end('{"g": '+JSON.stringify(Manifester.g)+', "r": '+JSON.stringify(Manifester.r)+'}');
+				res.end('{"not found"}');
 			});
-		}).catch(function(err) {
-			console.log("failure!");
-		
-			res.statusCode = 404;
-			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
-			res.setHeader('Access-Control-Allow-Methods', 'GET');
-			res.setHeader('Content-Type', 'application/json');
-			res.end('{"error: "RESOURCE NOT FOUND"}');
-		});
 	}
-	if (query.type == "proxy-marine") {
-			Manifester.getMarineTrafficOverview().then(function(result) {
+	if (query.type == 'proxy-marine') {
+		Manifester.getMarineTrafficOverview()
+			.then(function (result) {
 				res.statusCode = 200;
 
 				res.setHeader('Access-Control-Allow-Origin', '*');
 				res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 				res.setHeader('Access-Control-Allow-Methods', 'GET');
 				res.setHeader('Content-Type', 'application/json');
-		
-				console.log("proxy success!");
+
+				console.log('success');
 				res.end(JSON.stringify(result));
-			}).catch(function(err) {
-				console.log("failure!");
-		
+			})
+			.catch(function (err) {
+				console.log('failed');
+
 				res.statusCode = 404;
 				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+				res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 				res.setHeader('Access-Control-Allow-Methods', 'GET');
 				res.setHeader('Content-Type', 'application/json');
-				res.end('{"error: "RESOURCE NOT FOUND"}');
+				res.end('{"not found"}');
 			});
 	}
-	if (query.type == "aprsfi") {
-			Manifester.getAprsFi(query.id).then(function(result) {
+	if (query.type == 'aprsfi') {
+		Manifester.getAprsFi(query.id)
+			.then(function (result) {
 				res.statusCode = 200;
 
 				res.setHeader('Access-Control-Allow-Origin', '*');
 				res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 				res.setHeader('Access-Control-Allow-Methods', 'GET');
 				res.setHeader('Content-Type', 'application/json');
-		
-				console.log("proxy success!");
+
+				console.log('success');
 				res.end(JSON.stringify(result));
-			}).catch(function(err) {
-				console.log("failure!");
-		
+			})
+			.catch(function (err) {
+				console.log('failure!');
+
 				res.statusCode = 404;
 				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+				res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 				res.setHeader('Access-Control-Allow-Methods', 'GET');
 				res.setHeader('Content-Type', 'application/json');
 				res.end('{"error: "RESOURCE NOT FOUND"}');
 			});
 	}
 });
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+
