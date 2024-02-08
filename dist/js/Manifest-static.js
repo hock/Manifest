13548f46c6a3d7af2bb69ad99b111582c039db22
Matthew Hockenberry <hock@nyu.edu>
2024-02-08 16:15:46 -0500

    Rmoved Dist

--- dist/js/Manifest-static.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) { Start(); });
-
-/** Initializes the user interface for (mostly) static pages. **/
-function Start() {
-	document.querySelectorAll('#minfo-hamburger, #minfo').forEach(el => { 
-		el.addEventListener('click', (e) => { document.getElementById('minfodetail').classList.toggle('closed'); }); });	
-}
-
-//# sourceMappingURL=Manifest-static.js.map
