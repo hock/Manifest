bc75dd59c7e39013dfcc7a62f4d6c68ea5975ffd
Matthew Hockenberry <hock@nyu.edu>
2024-02-08 16:51:22 -0500

    Adds

---
+++ src/lib/js/util.js
@@ -0,0 +1,38 @@
+/* Manifest Utility Class */
+class ManifestUtilities {
+	constructor() {
+		this.markdowner = new showdown.Converter();	
+		
+		let customClassExt = {
+		    type: 'output',
+		    filter: function (text) {
+		        return text
+		            .replace(/<p>\[\.([a-z0-9A-Z\s]+)\]<\/p>[\n]?<(.+)>/g, `<$2 class="$1">`)
+		            .replace(/<(.+)>\[\.([a-z0-9A-Z\s]+)\]/g, `<$1 class="$2">`)            
+					.replace(/class="(.+)"/g, function (str) { if (str.indexOf("<em>") !== -1) { return str.replace(/<[/]?em>/g, '_'); } return str; });
+		    }
+		};
+		this.markdowner.addExtension(customClassExt);
+		
+		String.prototype.hashCode = function() {
+		  let hash = 0, i, chr;
+		  if (this.length === 0) return hash;
+		  for (let i = 0; i < this.length; i++) { chr = this.charCodeAt(i); hash = ((hash << 5) - hash) + chr; hash |= 0; }
+		  return Math.abs(hash);
+		};
+	}
+	static Linkify(str) { return str.replaceAll(ManifestUtilities.URLMatch(), '<a href=\"$1\">$1</a>').replaceAll(ManifestUtilities.ManifestMatch(), '<a class="manifest-link" href="$1">$1</a>'); }
+	static URLMatch() { return /(?![^<]*>|[^<>]*<\/(?!(?:p|pre|li|span)>))(https?:\/\/[^\s"]+)/gi; }
+	static ManifestMatch() { return /(?![^<]*>|[^<>]*<\/(?!(?:p|pre|li|span)>))(manifest?:\/\/[^\s"]+)/gi; }
+	static RemToPixels(rem) { return rem * parseFloat(getComputedStyle(document.documentElement).fontSize); }
+}
+
+function until(conditionFunction) {
+
+  const poll = resolve => {
+    if(conditionFunction()) resolve();
+    else setTimeout(_ => poll(resolve), 400);
+  };
+
+  return new Promise(poll);
+}
---
+++ src/services/services.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const { google } = require("googleapis");
+const aprsfi = require('./json/aprsfi.json');
+const maptiler = require('./json/maptiler.json');
+
+const hostname = 'hockbook.local';
+const port = 3000;
+
+const app = express();
+const cors = require('cors')
+
+app.use(cors())
+app.listen(port, hostname, () => { console.log(`Server running at ${hostname}:${port}/`); });
+
+
+app.get('/', (req, res) => {
+	console.log("Requested {/}");
+	res.send("Manifest Web Service v<%= mversion %>"); 
+});
+
+app.get('/marinetraffic/', async (req, res) => {});
+
+app.get('/maptiler/:type', async (req, res) => {
+	console.log("Requested Map Tile: "+req.params.type);	
+	
+	const response = await fetch('https://api.maptiler.com/maps/'+req.params.type+'/style.json?key='+maptiler.key);
+	const data = await response.json();
+	
+	console.log(data);
+	
+	res.send(data);
+});
+
+app.get('/aprsfi/vessel/:vID', async (req, res) => {
+	console.log("Requested AprsFi Vessel "+req.params.vID);	
+	
+	const response = await fetch('https://api.aprs.fi/api/get?name=' + req.params.vID + '&what=loc&apikey=' + aprsfi.key + '&format=json', {
+		headers: {'User-Agent': 'manifest/0.2 (+https://manifest.supplystudies.com/)',}
+	});
+	const data = await response.json();
+	
+	console.log(data);
+	
+	res.send(data);
+});
+
+app.get('/smap/:smapID', async (req, res) => {
+	console.log("Requested SMAP "+req.params.smapID);	
+	
+	let smap = {};
+
+	const geo = await fetch('https://raw.githubusercontent.com/hock/smapdata/master/data/' + req.params.smapID + '.geojson');
+	smap.geo = await geo.json();	
+	const graph = await fetch('https://raw.githubusercontent.com/hock/smapdata/master/data/' + req.params.smapID + '.json');
+	smap.graph = await graph.json();
+	
+	console.log(smap);
+	
+	res.send(smap);
+});
+
+app.get('/gsheet/:sheetID', async (req, res) => {
+	console.log("Requested Gsheet "+req.params.sheetID);	
+	
+	const auth = new google.auth.GoogleAuth({
+	        keyFile: "./json/google.json", //the key file
+	        //url to spreadsheets API
+	        scopes: "https://www.googleapis.com/auth/spreadsheets", 
+	    });
+	
+	const authClientObject = await auth.getClient();
+	const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+	
+	  googleSheetsInstance.spreadsheets.get( 
+	    { spreadsheetId: req.params.sheetID, fields: "sheets/properties/title" },
+	    (error, result) => { if (error) { 
+			console.log('The API returned an error: ' + error); 
+			res.status(500).send('The API returned an error: ' + error);
+			return; }
+	      googleSheetsInstance.spreadsheets.values.batchGet(
+	        { spreadsheetId: req.params.sheetID, ranges: result.data.sheets.map(e => e.properties.title) },
+	        (error, sheetresult) => {  if (error) {  
+				console.log('The API returned an error: ' + error); 
+				res.status(500).send('The API returned an error: ' + error);
+				return; }
+	         
+			  const rows = sheetresult.data.values;
+			//  for (let r of rows) {
+				  console.log(sheetresult.data.valueRanges);
+				  res.send(sheetresult.data.valueRanges);
+				 // res.send(res.data.valueRanges);
+				  //}
+	          //console.log(JSON.stringify(res, rows, 2));
+	        }
+	      );
+	    }
+	  );
+});
