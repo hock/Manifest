09d5ba5710b93633177a97978e37ad1e5c13ee67
Matthew Hockenberry <hock@nyu.edu>
2024-02-12 11:30:23 -0500

    Update manifester.js

--- src/services/manifester.js
+++ src/services/manifester.js
@@ -27,7 +27,7 @@ app.get('/maptiler/:type', async (req, res) => {
 	console.log("Requested Map Tile: "+req.params.type);	
 	
 	const response = await fetch('https://api.maptiler.com/maps/'+req.params.type+'/style.json?key='+maptiler.key), {
-		headers: {'Origin': 'https://service.supplystudies.com/)',}
+		headers: {'Referer': 'https://service.supplystudies.com/)',}
 	});
 	const data = await response.json();
 	
@@ -100,15 +100,4 @@ app.get('/gsheet/:sheetID', async (req, res) => {
 	      );
 	    }
 	  );
-});
-
-app.get('/maptiler/:type', async (req, res) => {
-	console.log("Requested Map Tile: "+req.params.type);	
-	
-	const response = await fetch('https://api.maptiler.com/maps/'+req.params.type+'/style.json?key='+maptiler.key);
-	const data = await response.json();
-	
-	console.log(data);
-	
-	res.send(data);
 });
