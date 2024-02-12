84dce4f371468015a06083bbbc33971fe5d198be
Matthew Hockenberry <hock@nyu.edu>
2024-02-12 11:34:09 -0500

    Update manifester.js

--- src/services/manifester.js
+++ src/services/manifester.js
@@ -13,7 +13,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.listen(mserver.port, mserver.name, () => { console.log(`Server running at ${mserver.name}:${mserver.port}/`); });
+app.listen(mserver.port () => { console.log(`Server running at ${mserver.name}:${mserver.port}/`); });
 
 
 app.get('/', (req, res) => {
