1374f33d9e10b34849d7b9fededdbe84b453cf24
Matthew Hockenberry <hock@nyu.edu>
2024-02-12 11:57:16 -0500

    Update manifester.js

--- src/services/manifester.js
+++ src/services/manifester.js
@@ -17,8 +17,8 @@ app.listen(mserver.port, () => { console.log(`Server running at ${mserver.name}:
 
 
 app.get('/', (req, res) => {
-	console.log("Requested {/}");
-	res.send("Manifest Web Service v${mserver.version}"); 
+	console.log('Requested {/}');
+	res.send('Manifest Web Service v${mserver.version}'); 
 });
 
 app.get('/marinetraffic/', async (req, res) => {});
@@ -26,7 +26,7 @@ app.get('/marinetraffic/', async (req, res) => {});
 app.get('/maptiler/:type', async (req, res) => {
 	console.log("Requested Map Tile: "+req.params.type);	
 	
-	const response = await fetch('https://api.maptiler.com/maps/'+req.params.type+'/style.json?key='+maptiler.key), {
+	const response = await fetch('https://api.maptiler.com/maps/'+req.params.type+'/style.json?key='+maptiler.key, {
 		headers: {'Referer': 'https://service.supplystudies.com/',}
 	});
 	const data = await response.json();
